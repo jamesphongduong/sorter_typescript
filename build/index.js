55775614ff89d69c02d93d57744248872d78245e
James Duong <james@simble.io>
2019-11-10 21:31:25 +1100

    initial scripts

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('hi theres');
+var logSomething = function () {
+    console.log('somethjing');
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('hi theres');
+
+const logSomething = () => {
+  console.log('somethjing');
+};
