b96df3732e25dfeabe2ba07c78a09dfc1aa368af
James Duong <james@simble.io>
2019-11-11 11:41:40 +1100

    #87

--- build/index.js
+++ build/index.js
@@ -1,8 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
-var NumbersCollections_1 = require("./NumbersCollections");
-var numbersCollection = new NumbersCollections_1.NumbersCollection([10, 3, -5, 0]);
-var sorter = new Sorter_1.Sorter(numbersCollection);
+var CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
+console.log('charactersCollection', charactersCollection);
+console.log(charactersCollection);
+var sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
